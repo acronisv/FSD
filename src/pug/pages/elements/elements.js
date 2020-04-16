d42ef8e75a0a351665ceaf35bb8aa4a97fe8d24a
acronisv <acronisv@gmail.com>
2020-04-16 21:35:55 +0400

    Added 'checkbox-list' component

--- src/pug/pages/elements/elements.js
+++ src/pug/pages/elements/elements.js
@@ -6,6 +6,7 @@ import './../../components/calendar/calendar.js';
 import './../../../js/jquery.maskedinput.min.js';
 import './../../components/field-subscribe/field-subscribe.js';
 import './../../components/checkbox/checkbox.js';
+import './../../components/checkbox-list/checkbox-list.js';
 //1 пример
 $(function(){
     $("#masked-textfield").mask("99.99.9999", {placeholder: "ДД.ММ.ГГГГ" });
