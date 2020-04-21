31a1a5a5fc0621136ae66b97a74ddee724033437
acronisv <acronisv@gmail.com>
2020-04-21 04:45:59 +0400

    Added 'information' component

--- src/pug/pages/elements/elements.js
+++ src/pug/pages/elements/elements.js
@@ -7,6 +7,7 @@ import './../../../js/jquery.maskedinput.min.js';
 import './../../components/field-subscribe/field-subscribe.js';
 import './../../components/checkbox/checkbox.js';
 import './../../components/checkbox-list/checkbox-list.js';
+import './../../components/information/information.js';
 //1 пример
 $(function(){
     $("#masked-textfield").mask("99.99.9999", {placeholder: "ДД.ММ.ГГГГ" });
