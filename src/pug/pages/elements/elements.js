70d258f06c5c56358a9d4e8ebe94b6a73df40a1d
acronisv <acronisv@gmail.com>
2020-04-24 03:22:33 +0400

    Beta version of radiobuttons

--- src/pug/pages/elements/elements.js
+++ src/pug/pages/elements/elements.js
@@ -8,6 +8,7 @@ import './../../components/field-subscribe/field-subscribe.js';
 import './../../components/checkbox/checkbox.js';
 import './../../components/checkbox-list/checkbox-list.js';
 import './../../components/information/information.js';
+import './../../components/radio/radio.js';
 //1 пример
 $(function(){
     $("#masked-textfield").mask("99.99.9999", {placeholder: "ДД.ММ.ГГГГ" });
