7c329e8fb2c913d468c4659f207017e1079e470a
acronisv <acronisv@gmail.com>
2020-07-16 01:32:57 +0400

    Fix 'dropdown' and 'room-search', add new component 'registration'

--- src/pug/pages/cards/cards.js
+++ src/pug/pages/cards/cards.js
@@ -4,3 +4,7 @@ import './../../components/room-search/room-search.js';
 import './../../components/calendar/calendar.js';
 import './../../components/dropdown/dropdown.js';
 import './../../components/button/button.js';
+import './../../components/registration/registration.js';
+import './../../components/input/input.js';
+import './../../components/radio/radio.js';
+import './../../components/toggle/toggle.js';
