32489cd3818bdd13107b817d91942ff63a067d52
acronisv <acronisv@gmail.com>
2020-08-01 22:53:47 +0300

    Finish new component 'footer'

--- src/pug/pages/headers-footers/headers-footers.js
+++ src/pug/pages/headers-footers/headers-footers.js
@@ -2,3 +2,5 @@ import './headers-footers.scss';
 import './../../components/head/head.js';
 import './../../components/header/header.js';
 import './../../components/button/button.js';
+import './../../components/footer/footer.js';
+import './../../components/field-subscribe/field-subscribe.js';
