1882b15dd0a4af99225c812e7fd22cda96327728
acronisv <acronisv@gmail.com>
2020-09-08 01:23:00 +0300

    Refactor 'header', converts px to em, add mobile menu

--- src/pug/components/header/header.js
+++ src/pug/components/header/header.js
@@ -1,1 +1,8 @@
 import './header.scss';
+
+$(document).ready(function(){
+    $('.header__burger-btn').click(function(event){
+        $('.header__burger').toggleClass('header__burger--active');
+        $('.header__mobile-wrap').toggleClass('header__mobile-wrap--active')
+    })
+})
